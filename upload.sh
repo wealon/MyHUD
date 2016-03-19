b3b3099f7573afd8ab825869379a1046a8758efe
wealon <linxianliuzhixiang@163.com>
2016-03-19 19:06:38 +0800

    new version

--- upload.sh
+++ upload.sh
@@ -3,15 +3,12 @@
 # rm the exist file 
 rm mdj-makeup-ios.ipa
 echo 0. delete file ok
-echo \n
 
 git pull -q
 echo 1. pull ready
-echo \n
 
 mv MDJStudy.ipa  mdj-makeup-ios.ipa
 echo 2. rename ok
-echo \n
 
 git add .
 echo 3. add commited file ok
@@ -19,6 +16,6 @@ echo 3. add commited file ok
 git commit -m "new version"
 echo 4. commit ok
 
-git push origin $(git_current_branch)
+git push origin master
 echo 5. push ready
 echo 6. done!!!
