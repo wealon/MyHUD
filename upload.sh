665b326dee8d7da9c00c1c4d7a1157f80917018e
wealon <linxianliuzhixiang@163.com>
2016-03-19 18:55:38 +0800

    new version

--- upload.sh
+++ upload.sh
@@ -3,12 +3,15 @@
 # rm the exist file 
 rm mdj-makeup-ios.ipa
 echo 0. delete file ok
+echo \n
 
 git pull -q
 echo 1. pull ready
+echo \n
 
 mv MDJStudy.ipa  mdj-makeup-ios.ipa
 echo 2. rename ok
+echo \n
 
 git add .
 echo 3. add commited file ok
