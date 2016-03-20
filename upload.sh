693286d59ad3a45fa5a79d63dda2ded1de66bb0f
wealon <linxianliuzhixiang@163.com>
2016-03-20 09:27:15 +0800

    new version

--- upload.sh
+++ upload.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 
 # rm the exist file 
 rm mdj-makeup-ios.ipa
 echo "0. delete file ok\n"
 
-git pull -q
+ggpull
 echo "1. pull ready\n"
 
 mv MDJStudy.ipa  mdj-makeup-ios.ipa
@@ -16,5 +16,5 @@ echo "3. add files ok\n"
 git commit -m "new version"
 echo "4. commit ok\n"
 
-git push origin master
+ggpush
 echo "5. push ready\n  EveryOne is OK!!!!!\n"
